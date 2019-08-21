4fc48509094a1ee97d15653fd8a7a21e752cd169
yulioaj290 <yulioaj290@gmail.com>
2019-08-20 20:06:08 -0400

    Updated script with more options.

--- NPM_UPLOADER.sh
+++ NPM_UPLOADER.sh
@@ -1,117 +1,231 @@
 #!/bin/bash
-#
-# Copyright 2018 Yulio Aleman Jimenez (@yulioaj290)
-# 
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions 
-# are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in 
-# the documentation and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived 
-# from this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT 
-# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
-# THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# NPM_UPLOADER.sh
-# This script split a file into chunks, create a NPM package with each one, and finally upload each package to https://npmjs.com
-#
 
-echo "--------------------------------------------------"
-echo "|                                                |"
-echo "|                  NPM UPLOADER                  |"
-echo "|                                                |"
-echo "--------------------------------------------------"
-echo "|             Yulio Aleman Jimenez               |"
-echo "|                  @yulioaj290                   |"
-echo "--------------------------------------------------"
+# ================================================
+# FUNCTIONS
+# ================================================
+
+# Check if 7Zip is installed
+check_7zip_func(){
+    # Must return "/usr/bin/7z"
+    ZIP7_INSTALLED="$(echo `whereis 7z` | awk -F "7z: " '{print $2}' | awk '{print $1}')"
+    if [[ -z $ZIP7_INSTALLED ]]; then
+        echo "false"
+    else
+        echo "true"
+    fi
+}
+
+# Check if NPM is installed and loged in
+check_npm_func(){
+    # Must return "/usr/bin/npm"
+    NPM_INSTALLED="$(echo `whereis npm` | awk -F "npm: " '{print $2}' | awk '{print $1}')"
+    if [[ -z $NPM_INSTALLED ]]; then
+        echo "installed"
+    else
+        # NPM_LOGED_IN="$(echo `npm whoami` | awk '{print $2}')"
+        # if [[ $NPM_LOGED_IN == "ERR!" ]]; then
+        #     echo "logedin"
+        # else
+            echo "true"
+        # fi
+    fi
+}
 
 
-if [[ ! -e "remove.lock" ]]; then
+# ================================================
+# MAIN PROCESS
+# ================================================
 
-    FILE_NAME=$1
-    PREFIX_PKG=$2
-    SPLIT_WEIGHT=$3
-    ZIP_LEVEL=$4
+# STEP 1: Verify requirements
+#   * 7zip-full package installed
+#   * NPM installed and loged in
 
-    NPM_INIT_START='{
-      "name": "'
-    NPM_INIT_END='",
-      "version": "1.0.0",
-      "description": "",
-      "main": "index.js",
-      "author": "",
-      "license": "ISC"
-    }
-    '
+if [[ $(check_7zip_func) == "false" ]]; then
+    echo "[ ERROR ]: You must to install 7zip or 7zip-full package first."
+elif [[ $(check_npm_func) == "installed" ]]; then
+    echo "[ ERROR ]: You must to install NPM package first."
+# elif [[ $(check_npm_func) == "logedin" ]]; then
+    # echo "[ ERROR ]: You must loged into the online NPM Registry."
+elif [[ ! -e "remove.lock" ]]; then
 
-    if [[ $FILE_NAME = '' ]]; then
-        echo "[ ERROR ]: You must to provide the 'NAME' of the file or directory."
-        echo "  --> SYNTAX EXAMPLE: bash NPM_UPLOADER.sh video.mp4 video-clip 30 9"
-        echo "  --> PARAMETER EXAMPLE: video.mp4"
+    # ================================================
+    # STEP 2: Initialize variables
+    # ================================================
+    ARG_URL="false"     #   * url   : Download resource from URL
+    ARG_LINK="false"    #   * link  : Make all chunk npm packages as dependencies of the 1st chunk package
+    ARG_RM="false"      #   * rm    : Remove original resources
+    ARG_PUSH="false"    #   * push  : Publish to NPM Registry automatically
+    ARG_CRON="false"    #   * cron  : Set crontab task to automatically unpublish npm packages
+    ARG_UNZIP="false"   #   * unzip : Provide to the NPM pachages auto unzip functions
+    #   ==============================================
+    ARG_FILE=""         #   * file  : Resource name or URL
+    ARG_PREFIX=""       #   * prefix: Prefix to use in npm packages
+    ARG_WEIGHT=""       #   * weight: Chunks weight
+    ARG_ZIPLV=""        #   * ziplv : Compression level
+
+
+    # Init "package.json" file vars
+    # ================================================
+    NPM_FILE_NAME=''                                    # Name  : Name of the package
+    NPM_FILE_DEPS=''                                    # Deps  : Dependencies of the first package
+    NPM_FILE_UNZIP='"postinstall": "bash unzip.sh"'     # UnZip : Script for postinstall automatically unzip
+
+
+    # ================================================
+    # STEP 3: Check optional arguments
+    # ================================================
+
+    ARGS=(`echo "$*"`)
+
+    declare -i NUM_OPT_ARGS=0
+
+    for ARG in ${ARGS[*]}
+    do
+        case $ARG in
+            *-url*) 
+                NUM_OPT_ARGS=$NUM_OPT_ARGS+1
+                ARG_URL="true";;
+            *-link*) 
+                NUM_OPT_ARGS=$NUM_OPT_ARGS+1
+                ARG_LINK="true";;
+            *-rm*) 
+                NUM_OPT_ARGS=$NUM_OPT_ARGS+1
+                ARG_RM="true";;
+            *-push*) 
+                NUM_OPT_ARGS=$NUM_OPT_ARGS+1
+                ARG_PUSH="true";;
+            *-cron*) 
+                NUM_OPT_ARGS=$NUM_OPT_ARGS+1
+                ARG_CRON="true";;
+            *-unzip*) 
+                NUM_OPT_ARGS=$NUM_OPT_ARGS+1
+                ARG_UNZIP="true";;
+        esac
+    done
+
+    shift $NUM_OPT_ARGS
+
+    # ================================================
+    # STEP 4: Set and Validate required arguments
+    # ================================================
+
+    ARG_FILE=$1
+    ARG_PREFIX=$2
+    ARG_WEIGHT=$3
+    ARG_ZIPLV=$4
+
+    if [[ $ARG_FILE = '' ]]; then
+        echo "[ ERROR ]: You must to provide the \"NAME\" of the file or directory."
         exit 128
     else
-        if [[ ! -e "$FILE_NAME" ]]; then
-            echo "[ ERROR ]: The file or directory $FILE_NAME doesn't exist."
-            echo "  --> SYNTAX EXAMPLE: bash NPM_UPLOADER.sh video.mp4 video-clip 30 9"
+        if [[ ! -e "$ARG_FILE" && "$ARG_URL" == "false" ]]; then
+            echo "[ ERROR ]: The file or directory \"$ARG_FILE\" does not exist."
             exit 128
         fi
     fi
 
-    if [[ $PREFIX_PKG = '' ]]; then
-        echo "[ ERROR ]: You must to provide the 'PREFIX' of NPM Packages."
-        echo "  --> SYNTAX EXAMPLE: bash NPM_UPLOADER.sh video.mp4 video-clip 30 9"
-        echo "  --> PARAMETER EXAMPLE: video-clip"
+    if [[ $ARG_PREFIX = '' ]]; then
+        echo "[ ERROR ]: You must to provide the \"PREFIX\" of NPM Packages."
         exit 128
     fi
 
-    if [[ $SPLIT_WEIGHT = '' ]]; then
-        echo "[ INFO ]: Setting 'WHEIGHT' to 50."
-        SPLIT_WEIGHT=50
+    if [[ $ARG_WEIGHT = '' ]]; then
+        ARG_WEIGHT=50
     fi
 
-    if [[ $ZIP_LEVEL = '' ]]; then
-        echo "[ INFO ]: Setting 'COMPRESSION LEVEL' to 5."
-        ZIP_LEVEL=5
-    elif [[ $ZIP_LEVEL -lt 1 ]] || [[ $ZIP_LEVEL -gt 9 ]]; then
-        echo "[ ERROR ]: You must to provide the 'COMPRESSION LEVEL' of 7Zip Packages, between 1 and 9."
-        echo "  --> SYNTAX EXAMPLE: bash NPM_UPLOADER.sh video.mp4 video-clip 30 9"
-        echo "  --> PARAMETER EXAMPLE: 9"
-        exit 128
+    if [[ $ARG_ZIPLV = '' ]]; then
+        ARG_ZIPLV=5
     fi
 
+    if [[ "$ARG_UNZIP" == "true" && "$ARG_LINK" == "false" ]]; then
+        echo "[ ERROR ]: To provide Auto UnZip functions, you need to link dependencies with the \"-link\" option."
+    fi
+
+
+    # ================================================
+    # STEP 5: Sumarize arguments values
+    # ================================================
+
+    echo "=================================================="
+    echo "[ Resource Name ] ........... $ARG_FILE"
+    echo "[ Resource Weight ] ......... $(echo `du -hs $ARG_FILE` | awk '{print $1}')""B"
+    echo "[ Package Prefix ] .......... $ARG_PREFIX"
+    echo "[ Weight of Chunks ] ........ $ARG_WEIGHT""MB"
+    echo "[ Compression Level ] ....... $ARG_ZIPLV"
     echo "--------------------------------------------------"
-    echo "[ File Name ]: $FILE_NAME"
-    echo "[ Package Prefix ]: $PREFIX_PKG"
-    echo "[ Weight of Chunks ]: $SPLIT_WEIGHT"" MB"
+    echo "[ Resource from URL ] ....... $ARG_URL"
+    echo "[ Link Dependencies ] ....... $ARG_LINK"
+    echo "[ Remove Resources ] ........ $ARG_RM"
+    echo "[ Publish to NPM ] .......... $ARG_PUSH"
+    echo "[ Unpublish with CRON ] ..... $ARG_CRON"
+    echo "[ Auto UnZip Functions ] .... $ARG_UNZIP"
+    echo "=================================================="
 
-    FILE_WEIGHT="$(echo `du $FILE_NAME` | cut -d' ' -f1)"
+
+    # ================================================
+    # STEP 6: Download resource from external URL
+    # ================================================
+
+    if [[ "$ARG_URL" == "true" ]]; then
+        if [[ -e "$ARG_FILE" ]]; then
+            echo "[ ERROR ]: The file or directory \"$ARG_FILE\" already exist."
+            echo "=================================================="
+            exit 128
+        elif [[ -e "$ARG_PREFIX" ]]; then
+            echo "[ ERROR ]: The file or directory \"$ARG_PREFIX\" already exist."
+            echo "=================================================="
+            exit 128
+        else
+            echo "[ INFO ]: Downloading \"$ARG_FILE\" from external URL..."
+            echo "=================================================="
+        
+            wget -O "$ARG_PREFIX" --quiet "$ARG_FILE"
+
+            ARG_FILE=$ARG_PREFIX
+        fi
+    fi
+
+
+    # ================================================
+    # STEP 7: Check number of chunks
+    # ================================================
+
+    FILE_WEIGHT="$(echo `du $ARG_FILE` | cut -d' ' -f1)"
 
     WEIGHT_IN_MB="$(( ($FILE_WEIGHT + (1024 - 1) ) / 1024 ))"
 
-    echo "[ File Weight ]: $WEIGHT_IN_MB"" MB"
-    echo "[ Compression Level ]: $ZIP_LEVEL"
-    echo "--------------------------------------------------"
+    CHUNKS_NUM="$(( ($WEIGHT_IN_MB + ( $ARG_WEIGHT - 1) ) / $ARG_WEIGHT ))"
 
-    CHUNKS_NUM="$(( ($WEIGHT_IN_MB + ( $SPLIT_WEIGHT - 1) ) / $SPLIT_WEIGHT ))"
 
-    echo "Splitting file into $CHUNKS_NUM chunks ..."
-    echo "--------------------------------------------------"
+    # ================================================
+    # STEP 8: Compressing and splitting resource
+    # ================================================
 
-    7za a -t7z -m0=lzma -mx=$ZIP_LEVEL -ms=on -v"$SPLIT_WEIGHT"m $PREFIX_PKG.7z $FILE_NAME
+    echo "[ INFO ]: Splitting file [ $ARG_FILE ] into [ $CHUNKS_NUM ] chunks ..."
 
-    echo "--------------------------------------------------"
-    echo "Removing original file ..."
-    echo "--------------------------------------------------"
+    7za a -t7z -m0=lzma -mx=$ARG_ZIPLV -ms=on -v"$ARG_WEIGHT"m $ARG_PREFIX.7z $ARG_FILE
 
-    rm -rf $FILE_NAME
+    echo "=================================================="
+
+    # ================================================
+    # STEP 9: Removing original resource
+    # ================================================
+
+    if [[ "$ARG_RM" == "true" ]]; then
+        echo "[ INFO ]: Removing original file [ $ARG_FILE ] ..."
+        echo "=================================================="
+
+        rm -rf $ARG_FILE
+    fi
+
+
+    # ================================================
+    # STEP 10: Build package dependencies
+    # ================================================
+
+    FIRST_PKG_DEPS=''               # Store package names and versions in JSON format [less first]
+    ZIP_PKG_LIST=''                 # Store zip package names separated by spaces
+    UNZIP_SCRIPT="#!/bin/bash \n"   # Store all command of the UnZip script
 
     for (( i=1 ; i <= $CHUNKS_NUM ; i++ ))
     do
@@ -124,48 +238,147 @@ if [[ ! -e "remove.lock" ]]; then
             CHUNK_ITEM_NUM="$i"
         fi
 
-        CHUNK_DIR_NAME="$PREFIX_PKG""_""$CHUNK_ITEM_NUM"
+        CHUNK_DIR_NAME="$ARG_PREFIX""_""$CHUNK_ITEM_NUM"
+
+        # unzip script
+        CURRENT_PKG="$ARG_PREFIX.7z.$CHUNK_ITEM_NUM"
+        ZIP_PKG_LIST="$ZIP_PKG_LIST $CURRENT_PKG"
+
+        if [[ $i -ne 1 ]]; then
+            FIRST_PKG_DEPS="$FIRST_PKG_DEPS"'"'$CHUNK_DIR_NAME'": "1.0.0"'
+            
+            if [[ $i -ne $CHUNKS_NUM ]]; then
+                FIRST_PKG_DEPS="$FIRST_PKG_DEPS"',
+                '
+            fi
+
+
+            # unzip script
+            UNZIP_SCRIPT="$UNZIP_SCRIPT""mv node_modules/$CHUNK_DIR_NAME/$CURRENT_PKG ../ \n"
+        else
+            # unzip script
+            UNZIP_SCRIPT="$UNZIP_SCRIPT""mv $CURRENT_PKG ../ \n"
+        fi
+    done
+
+    # unzip script
+    UNZIP_SCRIPT="$UNZIP_SCRIPT""cd ../ \n"
+    UNZIP_SCRIPT="$UNZIP_SCRIPT""7z x $ARG_PREFIX.7z.001 \n"
+    UNZIP_SCRIPT="$UNZIP_SCRIPT""rm -rf $ZIP_PKG_LIST \n"
+    UNZIP_SCRIPT="$UNZIP_SCRIPT""echo \"===================================\" \n"
+    UNZIP_SCRIPT="$UNZIP_SCRIPT""echo \"Remember to unpublish all pachages from 'NPMJS.com'.\" \n"
+    UNZIP_SCRIPT="$UNZIP_SCRIPT""echo \"Remember to delete the '${ARG_PREFIX}_001' package folder.\" \n"
+    UNZIP_SCRIPT="$UNZIP_SCRIPT""echo \"===================================\" \n"
+
+    # Setting "dependencies" of the first package to use in "package.json"
+    NPM_FILE_DEPS=$FIRST_PKG_DEPS
+
+
+    # ================================================
+    # STEP 11: Build, Publish and Remove NPM chunk packages
+    # ================================================
+
+    for (( i=1 ; i <= $CHUNKS_NUM ; i++ ))
+    do
+
+        if [[ $i -lt 10 ]]; then
+            CHUNK_ITEM_NUM="00$i"
+        elif [[ $i -ge 10 ]] && [[ $i -lt 100 ]]; then
+            CHUNK_ITEM_NUM="0$i"
+        else
+            CHUNK_ITEM_NUM="$i"
+        fi
+
+        # Building name of the package based on "prefix" and "chunk number"
+        CHUNK_DIR_NAME="$ARG_PREFIX""_""$CHUNK_ITEM_NUM"
+
+        # Setting "package name" to use in "package.json"
+        NPM_FILE_NAME=$CHUNK_DIR_NAME
+
+        echo "     [*]: Processing NPM chunk Package No. $i [ $CHUNK_DIR_NAME ]"
 
         mkdir $CHUNK_DIR_NAME
 
-        mv $PREFIX_PKG.7z.$CHUNK_ITEM_NUM $CHUNK_DIR_NAME/
+        mv $ARG_PREFIX.7z.$CHUNK_ITEM_NUM $CHUNK_DIR_NAME/
         
         cd $CHUNK_DIR_NAME
 
-        echo "$NPM_INIT_START""$CHUNK_DIR_NAME""$NPM_INIT_END" >> package.json
 
-        npm publish
+        if [[ $i -eq 1 ]]; then
+            if [[ "$ARG_LINK" == "false" ]]; then
+                NPM_FILE_DEPS=''
+            fi
 
-        echo "  --> Uploaded Package No. $i: $CHUNK_DIR_NAME"
+            # unzip script
+            if [[ "$ARG_UNZIP" == "true" ]]; then
+                echo -e $UNZIP_SCRIPT >> unzip.sh
+            else
+                NPM_FILE_UNZIP=''
+            fi
+
+        else
+            NPM_FILE_DEPS=''
+            NPM_FILE_UNZIP=''
+        fi
+        
+        NPM_FILE='{
+            "name": "'$NPM_FILE_NAME'",
+            "version": "1.0.0",
+            "description": "",
+            "main": "index.js",
+            "author": "",
+            "license": "ISC",
+            "dependencies": {
+                '$NPM_FILE_DEPS'
+            },
+            "scripts": {
+                '$NPM_FILE_UNZIP'
+            }
+        }'
+
+        echo "$NPM_FILE" >> package.json
+
+        echo "        :.. Created package ..."
+
+        if [[ "$ARG_PUSH" == "true" ]]; then
+            npm publish
+            
+            echo "        :.. Uploaded package to \"npmjs.com\" registry ..."
+        fi
 
         cd ../
 
         echo $CHUNK_DIR_NAME >> remove.lock
+        
+        if [[ "$ARG_RM" == "true" ]]; then
+            echo "        :.. Removed local package ..."
 
-        rm -rf $CHUNK_DIR_NAME
+            rm -rf $CHUNK_DIR_NAME
+        fi
 
-        echo "  --> Removed Package No. $i"
         echo "--------------------------------------------------"
     done
 
-    echo "[ INFO ]: File uploaded to npmjs.com successfully!!!"
-    echo "--------------------------------------------------"
+    echo "[ INFO ]: Processed [ $CHUNKS_NUM ] NPM chunk packages"
+    echo "=================================================="
+
+    echo "success"
 
 else
 
-    echo "--------------------------------------------------"
+    echo "=================================================="
     echo "[ INFO ]: Preparing to unpublish NPM Packages"
-    echo "--------------------------------------------------"
+    echo "=================================================="
 
     for PACKAGE in $(cat remove.lock);
     do
         npm unpublish --force $PACKAGE
 
-        echo "  --> Unpublided Package No. $i: $PACKAGE"
+        echo "     [*]: Unpublided Package: $PACKAGE"
     done
 
     rm -rf remove.lock
 
-    echo "--------------------------------------------------"
+    echo "=================================================="
 
 fi
--- README.md
+++ README.md
@@ -1,51 +1,73 @@
-NPM Uploader
-===============================
-Script Bash for uploading NPM pachages to npmjs.com. 
+# NPM Uploader
 
-__You must pass 3 required arguments:__ _name_ of the file/directory to include on the packages, _prefix_ name of the packages, and _number_ of chunks/packages to upload.
+Script Bash for uploading NPM pachages to npmjs.com.
 
-This script is capable to upload all packages to __npmjs.com__ secuencially and to remove the same packages if it is executed again immediately, following the information on the __"remove.lock"__ file create previously.
+__You must pass 2 required arguments:__ 
 
-Requirements
-------------
+* __name__ of the file/directory (could be a public URL) to include on the packages and;
+* __prefix__ name of the packages.
 
-  * NodeJS 8.4.0 or higher.
-  * NPM 5.3.0 or higher.
-  * P7zip-full 16.02 or higher.
+This script is capable to upload all packages to __npmjs.com__ secuencially and to remove the same packages if it is executed again immediately, following the information on the __"remove.lock"__ file created previously.
+
+## Requirements
+
+----------------
+
+* NodeJS 8.4.0 or higher.
+* NPM 5.3.0 or higher.
+* P7zip-full 16.02 or higher.
 
 To prepare the environment for the script follow this steps (based on Ubuntu 14.04):
 
-  0. (Optional) Update your linux repositories and upgrade all packages.
-  ```bash
-  $ sudo apt-get update
-  $ sudo apt-get upgrade
-  ```
+0. (Optional) Update your linux repositories and upgrade all packages.
 
-  1. Install p7zip-full on your system, it is neccesary to compress and divide in chunks all the file/directory included.
-  ```bash
-  $ sudo apt-get install p7zip-full
-  ```
-
-  2. Install NodeJS.
-  ```bash
-  $ sudo apt-get install nodejs
-  ```
-
-  3. Execute the script.
-
-  ```bash
-  $ bash NPM_UPLOADER.sh video.mp4 video-clip 30
-  ```
-  
-Parts of the Script command execution
----------------------------------------
-  
 ```bash
-$ bash NPM_UPLOADER.sh video.mp4 video-clip 30 9
+$ sudo apt-get update
+$ sudo apt-get upgrade
+```
+
+1. Install p7zip-full on your system, it is neccesary to compress and divide in chunks all the file/directory included.
+
+```bash
+$ sudo apt-get install p7zip-full
+```
+
+2. Install NodeJS.
+
+```bash
+$ sudo apt-get install nodejs
+```
+
+3. Execute the script.
+
+```bash
+$ bash NPM_UPLOADER.sh -link -unzip video.mp4 video-clip 50 9
 ```
   
-  * __NPM_UPLOADER.sh__: Name of the script file. __Required__.
-  * __video.mp4__: Name of the file to upload to npmjs.com as package(s). __Required__.
-  * __video-clip__:  Name _prefix_ of the packages to upload. __Required__.
-  * __30__: Upper limit of the size of the packages to upload. It must be expressed in MegaBytes (MB). __Required__.
-  * __9__: Level of compression of the packages to upload. It must be between 1 _(min compression)_ and 9 _(max compression)_.
+## Usage of the Script
+
+------------------------
+
+```bash
+$ bash NPM_UPLOADER.sh -link -unzip -rm -push video.mp4 video-clip 50 9
+```
+  
+```bash
+$ bash NPM_UPLOADER.sh -url -link -unzip -rm http://mi.website.com/video.mp4 video-clip 50 9
+```
+
+### Required arguments
+
+* __NPM_UPLOADER.sh__: Name of the script file. __Required__.
+* __video.mp4__: Name of the file to upload to npmjs.com as package(s). __Required__.
+* __video-clip__:  Name _prefix_ of the packages to upload. __Required__.
+* __30__: Upper limit of the size of the packages to upload. It must be expressed in MegaBytes (MB). __Required__.
+* __9__: Level of compression of the packages to upload. It must be between 1 _(min compression)_ and 9 _(max compression)_.
+
+### Optional arguments
+
+* __-url__: Indicate that the file must be downloaded from the given URL _(http://mi.website.com/video.mp4),_ and then build package.
+* __-link__: Include packages from 2nd to n(th) as dependencies of the 1st.
+* __-rm__: Remove all original files automatically.
+* __-push__: Publish all packages to 'npmjs.com' automatically. It requires to be loged in NPM in current terminal.
+* __-unzip__: Include a script into the 1st package to automatically UnZip the packages. Must be used with the '-link' option.
